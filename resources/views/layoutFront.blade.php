5ff943da79293d17c89706de5de6e4b2dc73cd69
Santiago Ruiz <sanruiz1003@gmail.com>
2016-07-07 13:10:39 -0500

    Instalacion autoprefixer y subida de imagenes

--- resources/views/layoutFront.blade.php
+++ resources/views/layoutFront.blade.php
@@ -7,30 +7,28 @@
     @yield('styles')
 </head>
 <body>
-    <header class="Header">
-        <video src="" class="VideoBackground" loop>
-           <source src="{{asset('video1.mp4')}}" type="video/mp4">
-            <source src="{{asset('video1.ogg')}}" type="video/ogg">
+    <!--<video src="" class="VideoBackground" loop>
+           <source src="{asset('video1.mp4')}}" type="video/mp4">
+            <source src="{asset('video1.ogg')}}" type="video/ogg">
             Por favor actualice su navegador
-        </video>
-        <div class="Container">
-            <figure class="Logo">
-                <img src="" alt="cityconsumo">
-            </figure>
-            <nav>
-                <ul class="Socials">
-                    <li><a href="">Icono twitter</a></li>
-                    <li><a href="">Icono instagram</a></li>
-                    <li><a href="">Icono facebook</a></li>
-                </ul>
-                <ul class="Menu">
-                    <li><a href="">Postula tu servicio</a></li>
-                    <li><a href="">Registrate</a></li>
-                    <li><a href="">Entrar</a></li>
-                    <li><a href="">Icono Menu</a></li>
-                </ul>
-            </nav>
-        </div>
+    </video>-->
+    <header class="Header">
+        <figure class="Logo">
+            <img src="{{asset('img/logo.svg')}}" alt="Cityconsumo">
+        </figure>
+        <nav>
+            <ul class="Socials">
+                <li><a href="">Icono twitter</a></li>
+                <li><a href="">Icono instagram</a></li>
+                <li><a href="">Icono facebook</a></li>
+            </ul>
+            <ul class="Menu">
+                <li><a href="">Postula tu servicio</a></li>
+                <li><a href="">Registrate</a></li>
+                <li><a href="">Entrar</a></li>
+                <li><a href="">Icono Menu</a></li>
+            </ul>
+        </nav>
     </header>
     @yield('content')
     @yield('scripts')
