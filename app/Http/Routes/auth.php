81fab8f0fad83fce2a47ed6c05cf933c068aec9b
Juan2Ramos <juan2ramos>
2016-07-07 22:11:20 -0500

    register

--- app/Http/Routes/auth.php
+++ app/Http/Routes/auth.php
@@ -8,9 +8,9 @@ Route::get('registro', function(){
     return view('auth.register');
 });
 
-Route::get('facebook/authorize', function() {
+Route::get('facebook/authorize', ['as' => 'facebook', function() {
     return SocialAuth::authorize('facebook');
-});
+}]);
 Route::get('auth', function() {
     SocialAuth::login('facebook', function($user, $details) {
         $user->nickname = $details->nickname;
