acb8feec091668b4955bc06652383fbdc3b958cf
santo52 <sanruiz1003@gmail.com>
2016-08-27 18:25:22 -0500

    Migración desembolsos, compras

---
+++ database/migrations/2016_08_27_181411_create_outlays_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOutlaysTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('outlay', function(Blueprint $table){
+            $table->increments('id');
+            $table->integer('value');
+            $table->string('buys_id');
+            $table->integer('user_id')->unsigned();
+            $table->integer('provider_id')->unsigned();
+
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('provider_id')->references('id')->on('providers');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
