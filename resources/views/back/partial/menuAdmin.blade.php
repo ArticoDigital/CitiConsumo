cf52cd3d234f2ce653708d5b2e9d4c6ec4adbacc
Juan2Ramos <juan2ramos>
2016-08-27 18:35:54 -0500

    postula tu servicio a usuarios administradores

--- resources/views/back/partial/menuAdmin.blade.php
+++ resources/views/back/partial/menuAdmin.blade.php
@@ -3,6 +3,7 @@
     <li><a href="{{route('showClient')}}">Usuarios Clientes </a></li>
     <li><a href="{{route('showProvider')}}">Usuarios Provedores</a></li>
     <li><a href="{{route('showProviderActive')}}">Usuarios Provedores por aprobar</a></li>
+    <li><a href="{{route('showProviderActive')}}">Proveedores eliminados</a></li>
 </ul>
 <div class="messages-success">
    <p>La informacion se ha actualizado correctamente</p>
