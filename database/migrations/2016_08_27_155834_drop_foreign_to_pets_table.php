45f2c87fec85e290de16c61fe712589d7957e294
santo52 <sanruiz1003@gmail.com>
2016-08-27 16:01:27 -0500

    Merge

--- database/migrations/2016_08_27_155834_drop_foreign_to_pets_table.php
+++ database/migrations/2016_08_27_155834_drop_foreign_to_pets_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class DropColumnsToPetsTable extends Migration
+class DropForeignToPetsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,7 +12,10 @@ class DropColumnsToPetsTable extends Migration
      */
     public function up()
     {
-        
+        Schema::table('pets', function(Blueprint $table){
+            $table->dropColumn('pet_size_id');
+            $table->dropColumn('pet_breed_id');
+        });
     }
 
     /**
