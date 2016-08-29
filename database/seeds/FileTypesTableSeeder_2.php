4c1f7c901171ef4722df2e9a52a8f5c10631cb4a
santo52 <sanruiz1003@gmail.com>
2016-08-29 13:50:56 -0500

    Modificacion seeder

--- database/seeds/FileTypesTableSeeder_2.php
+++ database/seeds/FileTypesTableSeeder_2.php
@@ -12,7 +12,31 @@ class FileTypesTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        FileType::create([
+            'name'=>'Cedula',
+            'description'=>'Cedula'
+        ]);
+
+        FileType::create([
+            'name'=>'Pasado judicial',
+            'description'=>'Pasado judicial'
+        ]);
+
+        FileType::create([
+            'name'=>'RUT',
+            'description'=>'RUT'
+        ]);
+
+        FileType::create([
+            'name'=>'Recibo de servicios',
+            'description'=>'Recibo de servicios'
+        ]);
+
+        FileType::create([
+            'name'=>'Cert. Cta bancaria',
+            'description'=>'Cert. Cta bancaria'
+        ]);
+
         FileType::create([
             'name'=>'Contraloria',
             'description'=>'Contraloria'
--- resources/views/back/usersProviderActive.blade.php
+++ resources/views/back/usersProviderActive.blade.php
@@ -54,13 +54,10 @@
                 <td class="center">{{$provider->user->cellphone}}</td>
                 <td class="center">
                     <a href="#" data-id="{{$provider->id}}" data-name="{{$provider->user->name}}" class="Button-table viewFiles">Ver</a>
-                    @if(isset($corregir)) <!-- ************************* Hay un error ************************-->
-                        @foreach($provider->providerFiles as $files)
-                            <input type="hidden" data-type="{{$files->fileType->name}}"
-                                   class="files-{{$provider->id}}" value="{{$files->name}}">
-
+                    @foreach($provider->providerFiles as $files)
+                        <input type="hidden" data-type="{{$files->fileType->name}}"
+                               class="files-{{$provider->id}}" value="{{$files->name}}">
                         @endforeach
-                    @endif
                 </td>
                 <td data-th="Price" class="center">
                     <a href="#" data-user="{{$provider->id}}" data-action="{{route('enabledProvider')}}"
