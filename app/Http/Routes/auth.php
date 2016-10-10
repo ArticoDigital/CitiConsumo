f867790f0a1273a66361f7177e3d51aa4223bdd6
Juan2Ramos <juan2ramos@gmail.com>
2016-10-09 19:25:18 -0500

    user

--- app/Http/Routes/auth.php
+++ app/Http/Routes/auth.php
@@ -38,7 +38,7 @@ Route::get('facebook/authorize', [
 
 Route::get('auth', function () {
     SocialAuth::login('facebook', function ($user, $details) {
-        dd($user);
+        dd($user->email);dd($user);
         if (!$user->exists) {
             $user->email = $details->email;
             $user->name = $details->full_name;
