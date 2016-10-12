fe4b6c96f51b3bb548c8513d2cb15cd9bd020814
santo52 <sanruiz1003@gmail.com>
2016-10-12 06:10:37 -0500

    corrección subir archivos

--- public/js/uploadfiles.js
+++ public/js/uploadfiles.js
@@ -1,83 +1,47 @@
- $('.DropFiles-inside').on('dragenter click', function(e){
-            $(this).addClass('Drag');
-        }).on('dragleave dragend mouseout drop', function(e){
-            $(this).removeClass('Drag');
-        });
-
-        $(".drop-files-input").change(function() {
-            $( this ).parent().find( '.text_file' ).text(this.value);
-            $( this ).parent().find( '.rectangle' ).css("background-color","green");
-            $( this ).parent().css("border","dashed 2px green");
-        });
-
-  var $files = $('.drop-files-input'),
-    $output;
-/*
-$files.on('change', function () {
-
-    if (this.files[0].size < 2400000) {
-
-        $('.preload').removeClass("hidden");
-        uploadImage(this.files[0]);
-
-    } else {
-        alert("El tamaño de la imagen debe ser inferior a 2MB");
-    }
-
+$('.DropFiles-inside').on('dragenter click', function(e){
+    $(this).addClass('Drag');
+}).on('dragleave dragend mouseout drop', function(e){
+    $(this).removeClass('Drag');
 });
 
-*/
+var $files = $('.drop-files-input');
+
 $files.on("change", function (event) {
     var form_url = $(this).data("url");
     var identifier = $(this).attr('name');
-    //alert(identifier);
     var CSRF_TOKEN = $('input[name="_token"]').val();
     var fd = new FormData();
-    if(this.files[0].type != 'application/pdf' && this.files[0].type != 'image/jpeg' && this.files[0].type != 'image/jpg'  && this.files[0].type != 'image/png'){
-        $(this).val('')
-        alert('EL archivo debe ser un pdf, imagen jpg o png');
-        return
-    }
-    //$(this).parent().find('.preload').removeCalss("hidden");
-    $('.preload').removeClass("hidden");
-    
-    fd.append("file", this.files[0]);
-    fd.append("identifier", identifier);
-    fd.append("_token", CSRF_TOKEN);
-    jQuery.ajax({
-        url: form_url,
-        data: fd,
-        cache: false,
-        contentType: false,
-        processData: false,
-        type: 'POST',
-        success: function (data) {
-            if (data.success) {
-                $('#'+identifier+"Name").val(data.name);
-                //$( this ).parent().find( '.text_file' ).text(this.value);
-                //$('#'+identifier).parent().find('.text_file').text("Archivo Subido");
-                $('.preload').addClass("hidden");
-                /*PDFObject.embed(data.url, "#pdf", {
-                    page: 1,
-                    pdfOpenParams: {
-                        navpanes: 1,
-                        view: "FitH",
-                        pagemode: "thumbs"
-                    }
-                });*/
-
+    if(this.files[0].type == 'application/pdf' || this.files[0].type == 'image/jpeg' || this.files[0].type == 'image/jpg' || this.files[0].type == 'image/png') {
+        $('.preload').removeClass("hidden");
+        $( this ).parent().find( '.text_file' ).text(this.value);
+        $( this ).parent().find( '.rectangle' ).css("background-color","green");
+        $( this ).parent().css("border","dashed 2px green");
+        fd.append("file", this.files[0]);
+        fd.append("identifier", identifier);
+        fd.append("_token", CSRF_TOKEN);
+        $.ajax({
+            url: form_url,
+            data: fd,
+            cache: false,
+            contentType: false,
+            processData: false,
+            type: 'POST',
+            success: function (data) {
+                if (data.success) {
+                    $('#' + identifier + "Name").val(data.name);
+                    $('.preload').addClass("hidden");
+                }
+                else if (!data.success) {
+                    $('#' + identifier).parent().find('.rectangle').css("background-color", "red");
+                    $('#' + identifier).parent().find('.text_file').text(data.name);
+                    $('#' + identifier).parent().css("border", "dashed 2px red");
+                    $('.preload').addClass("hidden");
+                }
             }
-             if (!data.success) {
-                $('#'+identifier).parent().find( '.rectangle' ).css("background-color","red");
-                $('#'+identifier).parent().find( '.text_file' ).text(data.name);
-                $('#'+identifier).parent().css("border","dashed 2px red");
-                $('.preload').addClass("hidden");
-             }
-
-            console.log(data);
-        }
-    });
-
-
+        });
+    }
+    else {
+        alert('El archivo debe ser un pdf, imagen jpg o png');
+    }
 });
 
