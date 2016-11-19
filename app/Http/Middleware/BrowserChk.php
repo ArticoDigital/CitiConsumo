950dbbd685fe977b9103c7fe3f524de2c9ac1ccc
santo52 <sanruiz1003@gmail.com>
2016-11-19 00:09:48 -0500

    get browser

--- app/Http/Middleware/BrowserChk.php
+++ app/Http/Middleware/BrowserChk.php
@@ -13,26 +13,15 @@ class BrowserChk
      * @param  \Closure  $next
      * @return mixed
      */
-    public function handle($request, Closure $next)
-    {
-/*
-        $browser = get_browser(null, true);
-        
-        if( $request->route()->getPath() != "nosoportado"){
-        // browser  -   version
-        if($browser['browser']=="IE"){
-                return redirect('nosoportado');
+    public function handle($request, Closure $next) {
 
+        $browser = get_browser(null, true);
+
+        if( $request->route()->getPath() != "nosoportado"){
+            if($browser['browser']=="IE")
+                return redirect('nosoportado');
         }
-        }
-  */
+
         return $next($request);
     }
 }
-
-/*
- * 3.49% + $900 minimo $2900
- * payu
- * $100 millones mensuales
- *
- * */
