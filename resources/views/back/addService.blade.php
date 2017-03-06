5ee4100210592396e9d588ebd1ce5a8646a2718b
Juan2Ramos <juan2ramos@gmail.com>
2017-03-06 10:08:54 -0500

    merge

--- resources/views/back/addService.blade.php
+++ resources/views/back/addService.blade.php
@@ -532,29 +532,14 @@
                     <div class="row  middle">
                         <input type="radio" id="answerIn" name="answer">
                         <label for="">Respondo en:</label>
-<<<<<<< HEAD
-                        <<<<<<< HEAD
-                        <select name="" id="answerInSelect" disabled>
-                            <option value="">30 minuntos</option>
-                            <option value="">1 hora</option>
-                            <option value="">1 Día</option>
-                            <option value="">1 semana</option>
-                        </select>
-                        =======
-                        <select class="js-example-basic-single" id="response_type" name="response_type">
+
+
+                        <select id="answerInSelect" disabled name="response_type_id">
                             @foreach($responseTypes as $responseType)
                                 <option value="{{$responseType->id}}">{{$responseType->name}}</option>
                             @endforeach
                         </select>
-                        >>>>>>> 59b36d1cfd9b226095a8faf4788e7373d285035e
-=======
 
-                        <select  id="answerInSelect" disabled name="response_type_id">
-                                @foreach($responseTypes as $responseType)
-                                    <option value="{{$responseType->id}}">{{$responseType->name}}</option>
-                                @endforeach
-                            </select>
->>>>>>> 473384117a943ebc35bc79003aa227ae84e74900
                     </div>
 
                 </div>
